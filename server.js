6574f39aadd71be1021073f11d6a31d9bea57257
vashli <mberi15@freeuni.edu.ge>
2020-05-22 21:14:32 +0400

    Add database connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const fastify = require('fastify')({ logger: true });
+const mysqlConnection = require("./db_connection");
 const userModule = new (require('./modules/user/user.module.js'))();
+
 
 fastify.get('/*', async (request, reply) => {
     return { hello: 'dima' }
@@ -9,18 +12,26 @@ fastify.get('/user', async (request, reply) => {
     return userModule.loginUser(request.query.name, request.query.pass)
 });
 
+fastify.get('/users', async (request, reply) => {
+    mysqlConnection.query("select * from app_users;", (err,rows,fields) =>{
+        if(!err){
+            reply.send(rows);
+            console.log((rows));
+        }else{
+            console.log(err);
+        }
+    });
+});
 
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(3000);
-        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+        await fastify.listen(80);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
     } catch (err) {
         fastify.log.error(err);
-        process.exit(1)
+        process.exit(1);
     }
 };
-
-
 
 start();
