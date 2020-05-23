a4f9fd8dcc58a13d42c9b2ec70af4e52c15adf51
Rogavactive <droga16@freeuni.edu.ge>
2020-05-23 20:52:50 +0400

    endpoint implemented, needed database integration

--- server.js
+++ server.js
@@ -26,6 +26,33 @@ fastify.get('/users', async (request, reply) => {
     });
 });
 
+fastify.get('/api/tournament/list', async  (request, reply) => {
+    console.log(request.query.fromNum);//int
+    console.log(request.query.toNum);//int
+    console.log(request.query.searchString);//string obviously
+    console.log(request.query.categories);//it is a list
+    console.log(request.query.fromDate);//timestamp
+    console.log(request.query.toDate);//timestamp
+    let replyData = [];
+    for (var i = 0; i < 10; i++ ){
+        replyData.push( //todo: replace with db call and its done.
+            {
+                id: 1,
+                title: 'This is a test contest lol',
+                body: '<h2>I am dummy data header for dummy contest.</h2>',
+                imageUrl: 'https://avatar.onlinesoccermanager.nl/03319541v1.png',
+                registrationStart: 1590160650706,
+                registrationEnd: 1590160850706,
+                nextContestStart: 1590161250706,
+                nextContestDuration: 180,
+                category: 'მათემატიკა'
+            }
+        );
+    }
+    reply.send(replyData)
+});
+
+
 // Run the server!
 const start = async () => {
     try {
