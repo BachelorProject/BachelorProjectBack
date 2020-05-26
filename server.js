1536d0e1c1f97d8d04ad6c249c6f6d356d67dfa2
vashli <mberi15@freeuni.edu.ge>
2020-05-26 23:05:27 +0400

    Port change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fastify.register(require('./routes/users'));
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(80);
+        await fastify.listen(3000);
         fastify.log.info(`server listening on ${fastify.server.address().port}`);
     } catch (err) {
         fastify.log.error(err);
