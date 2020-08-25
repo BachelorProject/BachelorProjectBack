d52b474d16084d0792dff306ee74e1de3cf5aecb
vashli <mberi15@freeuni.edu.ge>
2020-08-25 23:41:03 +0400

    Add getContests(v 1.1) - all filters, subjects, registeredUserCount Changed sync option for db objects

--- controllers/contest.js
+++ controllers/contest.js
@@ -1,15 +1,15 @@
 let Contest = require('./../models/contest');
+let Round = require('./../models/round');
+let Subject = require('./../models/subject');
+let User = require('./../models/user');
+let ContestRegisteredUser = require('./../models/contest_registered_user');
+let ContestSubject = require('./../models/contest_subject');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 module.exports = {
-    secret: async (request, reply) => {
-        console.log('SECTRET ------------I managed to get here!');
-        reply.send('SECTRET');
-    },
-
     createContest: async (request, reply) => {
-        console.log('SECTRET ------------I managed to get here!');
+        console.log('createContest ------------I managed to get here!');
 
         let newContest = {
             title: request.body.title,
@@ -48,34 +48,29 @@ module.exports = {
             searchString = '';
         }
 
-        let options = {where:{
+        let options = {
+            where:{
                 [Op.or]: {
                     description: {[Op.like]: `%${searchString}%`},
                     title: {[Op.like]: `%${searchString}%`}
                 }
             },
+            include: [
+                {
+                    model: Subject,
+                    required: false
+
+                },
+                {
+                    model: Round,
+                    required: false
+                }
+            ],
+            // attributes: ['contests.*', 'contestRegisteredUsers.*', [Sequelize.fn('COUNT', 'contestRegisteredUsers.userId'), 'userCount']],
             limit: limit,
             offset: (page - 1) * limit,
-
         };
 
-        // TODO for subjects filter
-        // where: {
-        //     $or: [
-        //         {'$B.userId$' : 100},
-        //         {'$C.userId$' : 100}
-        //     ]
-        // },
-        // include: [{
-        //     model: B,
-        //     required: false
-        //
-        // }, {
-        //     model: C,
-        //     required: false
-        // }]
-
-        // ???
         if (registrationIsOn === 'true') {
             options.where.status = ['N' , 'P']; // TODO statuses
         }else{
@@ -83,14 +78,94 @@ module.exports = {
                 [Op.not]: ['N', 'P']
             };
         }
-
         if (createdByMe === 'true'){
-            options.where.CreateUserId = request.body.userId; // TODO set userId or user to all authed methods
+            options.where.CreateUserId = request.user.dataValues.id; // TODO set userId or user to all authed methods
         }
 
+        if (subjects){
+            options.include[0].required = true;
+            options.include[0].where = { id: subjects.split()};
+        }
+
+        let contestsInfo = [];
+        let subjectsDict = {};
+        let countsDict = {};
+
+        // find general contest info
         Contest.findAll(options).then(function(contests){
             console.log(contests);
-            reply.send({error:false, message: 'contests list', data:contests});
+            let contestIds = [];
+            for (let i = 0; i < contests.length; i++) {
+                contestsInfo.push({
+                    id: contests[i].id,
+                    title: contests[i].title,
+                    body: contests[i].description,
+                    imageUrl: 'https://www.pngitem.com/pimgs/m/21-211363_slendytubbies-3-skin-url-hd-png-download.png', //TODO pictures
+                    registrationEnd: contests[i].registrationEnd, // TODO to long -- > .getTime(),
+                    nextContestStart: null, //todo
+                    nextContestDuration: null, //todo
+                    subjects: [],
+                    registeredCount: null
+                });
+                contestIds.push(contests[i].id)
+            }
+
+            // find registered users count
+            ContestRegisteredUser.findAll({
+                group: ['contestId'],
+                attributes: ['contestId', [Sequelize.fn('COUNT', 'contestId'), 'UserCount']],
+            }).then(function(count){
+                for (let i = 0; i < count.length; i++) {
+                    let el = count[i].toJSON()
+                    countsDict[el.contestId] = el.UserCount;
+                }
+
+                // get all subjects list for each contest
+                Contest.findAll({
+                    where: {
+                        id: contestIds
+                    },
+                    include: {
+                        model: Subject,
+                        required: true
+                    }
+                }).then(function(contestWithSubjects){
+
+                    for (let i = 0; i < contestWithSubjects.length; i++) {
+                        let subjects = [];
+                        for (let j = 0; j < contestWithSubjects[i].subjects.length; j++) {
+                            subjects.push({
+                                id: contestWithSubjects[i].subjects[j].id,
+                                name: contestWithSubjects[i].subjects[j].subject,
+                                color_id: contestWithSubjects[i].subjects[j].colorId
+                            });
+                            subjectsDict[contestWithSubjects[i].id] = subjects;
+                        }
+                    }
+
+                    for (let i = 0; i < contestsInfo.length; i++) {
+                        if (countsDict[contestsInfo[i].id]){
+                            contestsInfo[i].registeredCount = countsDict[contestsInfo[i].id];
+                        } else{
+                            contestsInfo[i].registeredCount = 0;
+                        }
+                        if (subjectsDict[contestsInfo[i].id]){
+                            contestsInfo[i].subjects = subjectsDict[contestsInfo[i].id];
+                        } else{
+                            contestsInfo[i].subjects = [];
+                        }
+                    }
+                    reply.send({error:false, message: 'contests list', contestsInfo: contestsInfo});
+
+                });
+
+            });
+
+
+
+
+
+
         }).catch(function(err){
             console.log('Oops! something went wrong, : ', err);
             reply.code(500);
